b729b5f00e46b91fb3fccd813754d8c340e351d8
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-08-28 09:22:35 +0530

    constants

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,15 @@
+export const API_ROUTES = {
+    /* AUTH ROUTES */
+    POST_REGISTER_USER: '/api/user',
+    POST_LOGIN_USER: '/api/user/login',
+    POST_LOGOUT_USER: '/api/user/logout',
+
+    POST_LOGIN_USER_MFA: '/api/user/mfa',
+    POST_VERIFY_EMAIL: '/api/user/verify/',
+
+    /* PROFILE ROUTES */
+    GET_USER: '/api/user',
+    GET_USER_PROFILE: '/api/user/profile',
+    PATCH_USER_PROFILE: '/api/user/profile',
+    PATCH_USER_PREFS: '/api/user/prefs',
+};
