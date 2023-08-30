ef62bf5a3e966a3d3750fcf67b2d26b549f525ed
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-08-30 10:57:53 +0530

    📖 documentation

--- frontend/src/lib/dataStorage.ts
+++ frontend/src/lib/dataStorage.ts
@@ -1,15 +1,28 @@
 import { User } from "@hooks/user/useUser";
 import { STORAGE_KEYS } from "@constants/storage";
 
+/**
+ * Saves the user object to local storage.
+ *
+ * @param user - The user object to be saved.
+ */
 export function saveUser(user: User): void {
-  localStorage.setItem(STORAGE_KEYS.USER_QUERY, JSON.stringify(user));
+    localStorage.setItem(STORAGE_KEYS.USER_QUERY, JSON.stringify(user));
 }
 
+/**
+ * Retrieves the user object from local storage.
+ *
+ * @returns The user object if found, otherwise undefined.
+ */
 export function getUser(): User | undefined {
-  const user = localStorage.getItem(STORAGE_KEYS.USER_QUERY);
-  return user ? JSON.parse(user) : undefined;
+    const userJSON = localStorage.getItem(STORAGE_KEYS.USER_QUERY);
+    return userJSON ? JSON.parse(userJSON) : undefined;
 }
 
+/**
+ * Removes the user object from local storage.
+ */
 export function removeUser(): void {
-  localStorage.removeItem(STORAGE_KEYS.USER_QUERY);
+    localStorage.removeItem(STORAGE_KEYS.USER_QUERY);
 }
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,12 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines multiple class values into a single class string using `clsx`
+ * and then merges Tailwind CSS classes using `twMerge`.
+ *
+ * @param inputs - The class values to be combined.
+ * @returns A string containing merged class values.
+ */
+export function cn(...inputs: ClassValue[]): string {
+    const combinedClasses = clsx(inputs);
+    const mergedClasses = twMerge(combinedClasses);
+    return mergedClasses;
 }
 
+/**
+ * Generates initials from a given name by taking the first letter of each word.
+ *
+ * @param name - The input name from which to generate initials.
+ * @returns The initials generated from the input name.
+ */
 export function getInitials(name: string): string {
-  const words = name.trim().split(' ');
-  const initials = words.map(word => word[0].toUpperCase()).join('').slice(0, 2);
-  return initials;
+    const words = name.trim().split(' ');
+    const initials = words.map(word => word[0].toUpperCase()).join('').slice(0, 2);
+    return initials;
 }
