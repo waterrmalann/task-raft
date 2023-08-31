c82705c0ab517ae1b230cdd0e64f68677d112a96
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-08-31 19:35:21 +0530

    middlewares

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const isAuthenticated = asyncHandler(async (req, res, next) => {
+    let token;
+    token = req.cookies.jwt;
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error("not authorized, invalid token");
+        }
+    } else {
+        res.status(401);
+        throw new Error("not authorized, no token");
+    }
+});
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+    if (req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error("not authorized, no privillege");
+    }
+});
+
+export { isAuthenticated, isAdmin };
