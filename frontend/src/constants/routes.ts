19f349ce847612aeb37c1561afbe30fb996dcb29
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-09-03 13:43:03 +0530

    Update routes.ts

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -12,4 +12,45 @@ export const API_ROUTES = {
     GET_USER_PROFILE: '/api/user/profile',
     PATCH_USER_PROFILE: '/api/user/profile',
     PATCH_USER_PREFS: '/api/user/prefs',
+
+    AUTH: {
+        REGISTER_POST: '/api/user',
+        LOGIN_POST: '/api/user/login',
+        LOGOUT_POST: '/api/user/logout',
+        MFA_POST: '/api/user/login',
+    },
+    VERIFICATION: {
+        EMAIL_GET: (verificationCode: string, userId: string) => `/api/user/verify/${verificationCode}?uid=${userId}`,
+        PWD_GET: (verificationCode: string, userId: string) => `/api/user/forget/${verificationCode}?uid=${userId}`
+    },
+    PROFILE: {
+        GET: '/api/user/profile',
+        PATCH: '/api/user/profile',
+    },
+    PREFERENCES: {
+        PATCH: '/api/user/prefs'
+    },
+
+    BOARD: {
+        POST: '/api/boards',
+        PATCH: (boardId: string) => `/api/boards/${boardId}`,
+        DELETE: (boardId: string) => `/api/boards/${boardId}`,
+        GET: (boardId: string) => `/api/boards/${boardId}`,
+    },
+    COLLABORATOR: {
+        GENERATE_POST: (boardId: string) => `/api/boards/${boardId}/collaborators/`,
+        INVITE_GET: (boardId: string) => `/api/boards/${boardId}/collaborators/`,
+        PATCH: (boardId: string, collaboratorId: string) => `/api/boards/${boardId}/collaborators/${collaboratorId}`,
+        DELETE: (boardId: string, collaboratorId: string) => `/api/boards/${boardId}/collaborators/${collaboratorId}`,
+    },
+    LIST: {
+        POST: (boardId: string) => `/api/boards/${boardId}/lists`,
+        PATCH: (boardId: string, listId: string) => `/api/boards/${boardId}/lists/${listId}`,
+        DELETE: (boardId: string, listId: string) => `/api/boards/${boardId}/lists/${listId}`,
+    },
+    CARD: {
+        POST: (boardId: string) => `/api/boards/${boardId}/cards`,
+        PATCH: (boardId: string, cardId: string) => `/api/boards/${boardId}/cards/${cardId}`,
+        DELETE: (boardId: string, cardId: string) => `/api/boards/${boardId}/cards/${cardId}`,
+    },
 };
