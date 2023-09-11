c21d3d8d8126b262a2f18f2be68e876199bc8779
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-09-12 01:07:11 +0530

    reordered route priority

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -7,27 +7,24 @@ const router = express.Router();
 // CRUD operations on boards are only available to authenticated users.
 router.use(isAuthenticated);
 
-// Board
 router.post("/", boardController.createBoard);
-    router.patch("/:boardId", boardController.editBoard);
-    router.delete("/:boardId", boardController.deleteBoard);
-    router.get("/:boardId", boardController.getBoard);
-
-// Collaborators
-router.get("/:boardId/collaborators/invite", boardController.verifyCollaborator);
-router.post("/:boardId/collaborators/", boardController.inviteCollaborator);
-    router.patch("/:boardId/collaborators/:collaboratorId", boardController.editCollaborator);
-    router.delete("/:boardId/collaborators/:collaboratorId", boardController.removeCollaborator);
-
-// Lists
 router.post('/:boardId/lists', boardController.addList);
-    router.patch('/:boardId/lists/:listId', boardController.editList);
-    router.delete('/:boardId/lists/:listId', boardController.deleteList);
-
-// Cards
 router.post('/:boardId/cards', boardController.addCard);
-    router.patch('/:boardId/cards/:cardId', boardController.editCard);
-    router.put('/:boardId/cards/:cardId', boardController.moveCard);
-    router.delete('/:boardId/cards/:cardId', boardController.deleteCard);
+router.post("/:boardId/collaborators/", boardController.inviteCollaborator);
+
+router.patch("/:boardId/collaborators/:collaboratorId", boardController.editCollaborator);
+router.patch('/:boardId/lists/:listId', boardController.editList);
+router.patch('/:boardId/cards/:cardId', boardController.editCard);
+router.patch("/:boardId", boardController.editBoard);
+
+router.delete("/:boardId/collaborators/:collaboratorId", boardController.removeCollaborator);
+router.delete('/:boardId/lists/:listId', boardController.deleteList);
+router.delete('/:boardId/cards/:cardId', boardController.deleteCard);
+router.delete("/:boardId", boardController.deleteBoard);
+
+router.get("/:boardId/collaborators/invite", boardController.verifyCollaborator);
+router.get("/:boardId", boardController.getBoard);
+
+router.put('/:boardId/cards/:cardId', boardController.moveCard);
 
 export default router;
