8f6cf7f4e65d4b967686aafc26b078e19f0135aa
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-09-11 20:11:09 +0530

    Update HomeComponent.tsx

--- frontend/src/components/main/HomeComponent.tsx
+++ frontend/src/components/main/HomeComponent.tsx
@@ -3,6 +3,7 @@ import BoardCardComponent from "./BoardCardComponent";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { RxStar } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import { slugify } from "@/lib/utils";
 
 const HomeComponent = () => {
   const {data, isLoading} = useDashboard();
@@ -18,25 +19,41 @@ const HomeComponent = () => {
       </Alert>
       {isLoading && <p>Loading...</p>}
       <h1 className="text-xl font-bold my-5">Recent Boards</h1>
-      <div className="flex gap-3">
-        <Link to="/dash/boards/">
-          <div className="rounded-md border px-4 py-3 font-mono text-sm bg-white hover:bg-muted hover:cursor-pointer">
-          @user/slug
-          </div>
-        </Link>
-      </div>
+      {(data?.recentBoards.length || 0) > 0 ? (
+        <div className="flex gap-3">
+          {
+            data?.recentBoards.map(board => (
+              <Link key={board.boardId} to={`/dash/boards/${board.boardId}`}>
+                <div className="rounded-md border px-4 py-3 font-mono text-sm bg-white hover:bg-muted hover:cursor-pointer">
+                  @{board.createdBy}/{slugify(board.boardName)}
+                </div>
+              </Link>
+            ))
+          }
+        </div>
+      ) : (
+        <div className="py-5 text-center">
+            <p>Your recently opened boards will show up here.</p>
+        </div>
+      )}
 
       <h1 className="text-xl font-bold my-5">Your Workspaces</h1>
-      <div className="grid grid-cols-3">
+      {(data?.boards.length || 0) > 0 ? (
+        <div className="grid grid-cols-3">
         {data?.boards.map(board => (
-          <BoardCardComponent
+          <BoardCardComponent key={board._id}
             _id={board._id}
             title={board.title}
             description={board.description}
             updatedAt={board.updatedAt}
           />
         ))}
-      </div>
+        </div>
+      ) : (
+        <div className="py-5 text-center">
+            <p>Your boards will show up here. Create a new board to get started.</p>
+        </div>
+      )}
     </div>
   )
 }
