a91b9bc5b29dad7d31fe2c001de9db950eb6fc45
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-09-18 17:16:27 +0530

    <HomePage />

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,28 @@
-const HomePage = () => {
+import { FeatureListComponent } from "@components/landing/FeatureListComponent"
+import { FooterComponent } from "@components/landing/FooterComponent"
+import { HeroComponent } from "@components/landing/HeroComponent"
+import { MainContent } from "@components/landing/MainContent"
+import { NavbarComponent } from "@components/landing/NavbarComponent"
+// import { PricingComponent } from "@components/landing/PricingComponent"
+import { TestimonialComponent } from "@components/landing/TestimonialComponent"
+
+function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <NavbarComponent />
+
+      <HeroComponent />
+
+      <MainContent />
+      
+      <FeatureListComponent />
+      
+      <TestimonialComponent />
+      
+      {/* <PricingComponent /> */}
+      
+      <FooterComponent />
+    </>
   )
 }
 
