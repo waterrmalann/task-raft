65908ff87aecc4350e02133ff67c48a2ccbe6286
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-09-20 11:32:10 +0530

    Styled 404 page

--- frontend/src/pages/404/NotFoundPage.tsx
+++ frontend/src/pages/404/NotFoundPage.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@components/ui/button"
+import { Link } from "react-router-dom"
+
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="block text-2xl">404</h1>
+      <h2 className="block text-xl font-bold">Page Not Found</h2>
+      <Link to="/dash"><Button className="my-3">Go Back</Button></Link>
+    </div>
   )
 }
 
